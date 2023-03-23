4db66ee688ff70ecf3a18fe5d66745e37360cb8e
isaacmou <126214425+isaacmou@users.noreply.github.com>
2023-03-23 23:57:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function showEnglish() {
     document.getElementById("engVer").style.display = "";
     document.getElementById("chiVer").style.display = "none";
+    document.getElementById("engVerwithdistrict").style.display = "none";
     document.getElementById("toptitle").innerHTML = "Fitness Walking Tracks";
     document.getElementById("engInput").style.display = "";
     document.getElementById("chiInput").style.display = "none";
@@ -10,9 +11,20 @@ function showEnglish() {
 function showChinese() {
     document.getElementById("engVer").style.display = "none";
     document.getElementById("chiVer").style.display = "";
+    document.getElementById("engVerwithdistrict").style.display = "none";
     document.getElementById("toptitle").innerHTML = "健步行徑";
     document.getElementById("engInput").style.display = "";
     document.getElementById("chiInput").style.display = "none";
+}
+
+function location() {
+    document.getElementById("engVer").style.display = "none";
+    document.getElementById("chiVer").style.display = "none";
+    document.getElementById("engVerwithdistrict").style.display = "";
+    document.getElementById("toptitle").innerHTML = "Fitness Walking Tracks";
+    document.getElementById("engInput").style.display = "";
+    document.getElementById("chiInput").style.display = "none";
+
 }
 
 
