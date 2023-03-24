0c5c760470bd6641bf95103f8bd9e7746e1bdde5
isaacmou <126214425+isaacmou@users.noreply.github.com>
2023-03-24 12:54:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function showEnglish() {
-    document.getElementById("engVer").style.display = "";
+    document.getElementById("engVer").style.display = "none";
     document.getElementById("chiVer").style.display = "none";
     document.getElementById("engVerwithdistrict").style.display = "none";
     document.getElementById("toptitle").innerHTML = "Fitness Walking Tracks";
